7ccb2b21950c52b69ed10025bad97b7fe2bbce72
alban <alban.haka@stud.unifi.com>
2023-09-15 16:59:05 +0200

    google test

--- test/ShoppinItemTest.cpp
+++ test/ShoppinItemTest.cpp
@@ -1,8 +1,16 @@
 #include "gtest/gtest.h"
 #include "../ShoppingItem.h"
 
-TEST(ShoppingItem, DefaultConstructor) {
+TEST(ShoppingItem, Constructor) {
     ShoppingItem ogg("banana","frutto",2);
+    ASSERT_EQ("banana",ogg.getNome());
+    ASSERT_EQ("frutto",ogg.getCategoria());
+    ASSERT_EQ(2,ogg.getQuantita());
+}
+
+TEST(ShoppingItem, view) {
+    ShoppingItem ogg("banana","frutto",2);
+    ogg.view();
     ASSERT_EQ("banana",ogg.getNome());
     ASSERT_EQ("frutto",ogg.getCategoria());
     ASSERT_EQ(2,ogg.getQuantita());
