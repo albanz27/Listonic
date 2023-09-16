0b630a27d95369c2bfe63b471ac953d50b24dad5
alban <alban.haka@stud.unifi.com>
2023-09-16 16:17:48 +0200

    aggiunto file ShoppingListTest.cpp

---
+++ test/ShoppingListTest.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "../ShoppingItem.h"
+
+TEST(ShoppingItem, Constructor) {
+    ShoppingItem ogg("banana","frutto",2);
+    ASSERT_EQ("banana",ogg.getNome());
+    ASSERT_EQ("frutto",ogg.getCategoria());
+    ASSERT_EQ(2,ogg.getQuantita());
+}
