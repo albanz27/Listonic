129be97b40d6b54cac644ecb99900dab4f66fba5
alban <alban.haka@stud.unifi.com>
2023-09-16 16:33:23 +0200

    aggiornamento ShoppingListTest.cpp

--- test/ShoppingListTest.cpp
+++ test/ShoppingListTest.cpp
@@ -1,9 +1,7 @@
 #include "gtest/gtest.h"
-#include "../ShoppingItem.h"
+#include "../ShoppingList.h"
 
-TEST(ShoppingItem, Constructor) {
-    ShoppingItem ogg("banana","frutto",2);
-    ASSERT_EQ("banana",ogg.getNome());
-    ASSERT_EQ("frutto",ogg.getCategoria());
-    ASSERT_EQ(2,ogg.getQuantita());
+TEST(ShoppingList, Constructor) {
+    ShoppingList lista("spesa");
+    ASSERT_EQ("spesa",lista.getNome());
 }
