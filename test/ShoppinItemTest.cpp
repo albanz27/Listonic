52489f74141b84ccd8171e769f89c0c1009f1c9e
alban <alban.haka@stud.unifi.com>
2023-09-16 11:22:05 +0200

    aggiornamento ShoppinItemTest.cpp

--- test/ShoppinItemTest.cpp
+++ test/ShoppinItemTest.cpp
@@ -7,11 +7,3 @@ TEST(ShoppingItem, Constructor) {
     ASSERT_EQ("frutto",ogg.getCategoria());
     ASSERT_EQ(2,ogg.getQuantita());
 }
-
-TEST(ShoppingItem, view) {
-    ShoppingItem ogg("banana","frutto",2);
-    ogg.view();
-    ASSERT_EQ("banana",ogg.getNome());
-    ASSERT_EQ("frutto",ogg.getCategoria());
-    ASSERT_EQ(2,ogg.getQuantita());
-}
