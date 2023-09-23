8ca0daacbec65f0e01635ff33af4c47e10b2a8fa
alban <alban.haka@stud.unifi.com>
2023-09-23 19:51:54 +0200

    test addObject

--- test/ShoppingListTest.cpp
+++ test/ShoppingListTest.cpp
@@ -5,3 +5,17 @@ TEST(ShoppingList, Constructor) {
     ShoppingList lista("spesa");
     ASSERT_EQ("spesa",lista.getNome());
 }
+
+TEST(ShoppingList, addObject) {
+    ShoppingItem oggetto("Banana","FRUTTA",2);
+    ShoppingList lista("spesa");
+    lista.addObject(oggetto);
+
+    bool found = false;
+    for (const ShoppingItem& ogg : lista.oggetti) {
+        if (ogg.getNome() == "Banana") {
+            found = true;
+        }
+    }
+    ASSERT_TRUE(found);
+}
