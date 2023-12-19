01a66b4d59749f22315cdbbeb24599229b71471b
alban <alban.haka@stud.unifi.com>
2023-12-19 15:24:13 +0100

    test notBought,modQuantity

--- test/ShoppingListFixture.cpp
+++ test/ShoppingListFixture.cpp
@@ -29,7 +29,7 @@ TEST_F(ShoppingListFixture, Test_addObject){
 }
 
 // removeObject
-TEST_F(ShoppingListFixture, Test_removeOggetto) {
+TEST_F(ShoppingListFixture, Test_removeObject) {
     // Rimuovo un oggetto dalla lista
     ASSERT_TRUE(list1.removeObject("Banana"));
 
@@ -43,9 +43,30 @@ TEST_F(ShoppingListFixture, Test_stateObject) {
     ASSERT_TRUE(list1.stateObject("Gocciole", true));
     // Segno le Banane come objects da acquistare
     ASSERT_TRUE(list1.stateObject("Banana", false));
-
     // Cambio stato ad un oggetto non esistente
     ASSERT_FALSE(list1.stateObject("Uova", true));
 }
 
-// modQuantity
+TEST_F(ShoppingListFixture, Test_modQuantity) {
+    // aumenta la quantità di 10
+    ASSERT_TRUE(list1.modQuantity("Banana",10,1));
+
+    // diminuisce la quantità di 4
+    ASSERT_TRUE(list1.modQuantity("Gocciole",4,0));
+
+    // la quantità dell'oggetto è minore della quantità che si vuole decrementare
+    ASSERT_FALSE(list1.modQuantity("Insalata",10,0));
+
+    // modifico quantità di un oggetto non esistente
+    ASSERT_FALSE(list1.modQuantity("Mela",10,0));
+
+
+}
+
+TEST_F(ShoppingListFixture, Test_notBought) {
+    // somma degli oggetti non acquistati
+    ASSERT_EQ(list1.notBought(),8);
+}
+
+TEST_F(ShoppingListFixture, Test_subject) {
+}
